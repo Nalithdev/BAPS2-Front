8e9f8ede75530915ef01b14cf5906d502be6a5a4
tinkode92 <yanis.bekrarchouche@edu.devinci.fr>
2023-03-30 08:12:22 +0200

    🌟 sign-up & login page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Euclid Circular', Verdana, Geneva, Tahoma, sans-serif;
 }
 
-nav {
-  padding: 30px;
+html, body, #app {
+  width: 100%;
+  height: 100%;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+@font-face {
+  font-family: "Euclid Circular";
+  src: local("Euclid Circular A"),
+    url(./fonts/EuclidCircular/EuclidCircularRegular.ttf) format("truetype");
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+@font-face {
+  font-family: "Euclid Circular";
+  src: local("Euclid Circular A Bold"),
+    url(./fonts/EuclidCircular/EuclidCircularBold.ttf) format("truetype");
+  font-weight: 700;
 }
 </style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="page">
+    <img alt="Fid'Anthony logo" src="../assets/logo.png">
+    <h1>Bon retour <span>parmis nous </span>!</h1>
+    <form>
+      <input type="text" name="email" placeholder="example@gmail.com">
+      <input type="password" name="password" placeholder="Mot de passe">
+      <label for="rememberMe">
+        <input type="checkbox" name="rememberMe" id="rememberMe">
+        Me garder connecté
+      </label>
+      <button type="submit">Connexion</button>
+    </form>
+    <p>
+      Pas de compte ? <router-link to="/">Inscrivez vous</router-link>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class LogIn extends Vue {}
+</script>
+
+<style scoped>
+img {
+  margin-bottom: 20px;
+}
+
+.page {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 20px;
+  height: 100%;
+  padding: 80px 0;
+}
+
+h1 {
+  color: #3B434F;
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+span {
+  position: relative;
+  z-index: 1;
+}
+
+span::before {
+  content: "";
+  position: absolute;
+  top: 10px;
+  bottom: 2px;
+  right: -3px;
+  left: 2px;
+  z-index: -1;
+  transform: translate(-5px, 5px);
+  background-color: #FF866A;
+}
+
+input {
+  outline: none;
+  border: none;
+  padding: 2px 2px;
+  border-bottom: 1px solid #2823214d;
+  font-size: 16px;
+}
+
+a {
+  color: #FF5A34;
+}
+
+button {
+  background: none;
+  position: relative;
+  border: none;
+  color: #FB3D03;
+  width: 100%;
+  padding: 10px 0;
+  cursor: pointer;
+  border-radius: 20px;
+  z-index: 1;
+  font-weight: 700;
+  font-size: 16px;
+}
+
+button::after {
+  content: "";
+  position: absolute;
+  inset: 0;
+  border-radius: 20px;
+  background: linear-gradient(to right, #fb3d03, #05306e);
+  padding: 2px;
+  mask:
+     linear-gradient(#fff 0 0) content-box,
+     linear-gradient(#fff 0 0);
+  mask-composite: exclude;
+}
+
+label {
+  width: 100%;
+  color: #3B434F;
+  font-size: 12px;
+}
+
+input[type="checkbox"] {
+  margin-right: 2px;
+}
+</style>
